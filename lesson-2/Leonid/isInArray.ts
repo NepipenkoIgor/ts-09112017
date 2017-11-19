b42dfc691a0a61f4c66b81c84fb27acdfe57887a
Kuznetsov Leonid <kuznetsovlv1981@gmail.com>
2017-11-19 11:39:24 +0300

    Refactored isInArray

--- lesson-2/Leonid/isInArray.ts
+++ lesson-2/Leonid/isInArray.ts
@@ -1,1 +1,8 @@
 const isInArray = (arr: any[], ...rest: any[]): boolean => rest.every((elem: any): boolean => !~arr.indexOf(elem));
+
+console.log(isInArray([1, 2, 3]));
+console.log(isInArray([1, 2, 3], 2, 3));
+console.log(isInArray([1, 2, 3], 0));
+console.log(isInArray([1, 2, 3], 1, 2, 3, 4));
+console.log(isInArray([]));
+    
